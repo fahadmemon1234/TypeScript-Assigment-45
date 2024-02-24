528a39e7fe50d544c381dd655e87bb33b0b21225
fahadmemon1234 <fahadmemon131@gmail.com>
2024-02-24 14:37:50 +0500

    add file

--- main.js
+++ main.js
@@ -62,25 +62,78 @@
 // guestArr [guestArr.indexOf(canNotAttend)] = NewGuest;
 // guestArr.map((item)=> console.log(item));
 // // ----------Task 16------------
-var guestArr = ["Ali", "Ahmed", "Mehak", "Almas", "Bilal"];
-var canNotAttend = "Mehak";
-var NewGuest = "Fahad";
-guestArr[guestArr.indexOf(canNotAttend)] = NewGuest;
-console.log(guestArr);
-// guestArr.map((item)=> console.log(`Dear ${item}, I found a bigger dinner table so I am invited more peoples.`));
-// part 2 began
-var guestBeg = "Muizz";
-guestArr.unshift(guestBeg);
-console.log(guestArr);
-// part 3 Middle
-var guestMiddle = "Hamza";
-var MiddleIndex = guestArr.length / 3;
-guestArr.splice(MiddleIndex, 0, guestMiddle);
-console.log(guestArr);
-// part 4 append
-guestArr.push("Zeeshan");
-console.log(guestArr);
-// part 5
-guestArr.map(function (items) {
-    return console.log("Dear ".concat(items, " you are invited in the more people category on dinner"));
-});
+// let guestArr = ["Ali", "Ahmed", "Mehak", "Almas", "Bilal"];
+// let canNotAttend = "Mehak";
+// let NewGuest = "Fahad";
+// guestArr[guestArr.indexOf(canNotAttend)] = NewGuest;
+// console.log(guestArr);
+// // guestArr.map((item)=> console.log(`Dear ${item}, I found a bigger dinner table so I am invited more peoples.`));
+// // part 2 began
+// let guestBeg = "Muizz";
+// guestArr.unshift(guestBeg);
+// console.log(guestArr);
+// // part 3 Middle
+// let guestMiddle = "Hamza";
+// let MiddleIndex = guestArr.length / 3;
+// guestArr.splice(MiddleIndex, 0, guestMiddle);
+// console.log(guestArr);
+// // part 4 append
+// guestArr.push("Zeeshan");
+// console.log(guestArr);
+// // part 5
+// guestArr.map((items) =>
+//   console.log(
+//     `Dear ${items} you are invited in the more people category on dinner`
+//   )
+// );
+// // ----------Task 17------------
+// let guest = ["Ali", "Bilal", "Carry", "Dawood", "Hamza"];
+// console.log("Due to limited space, only two people can be invited for dinner.")
+// // Removing guest until only two names remain
+// while (guest.length > 2){
+//   const removeGuest = guest.pop();
+//   console.log(`Sorry, ${removeGuest}, you're no longer invited to dinner.`)
+// }
+// // Print Invitations to the remaining two guest
+// let guests = ["Ali", "Bilal"];
+// guests.forEach((guests) =>
+//   console.log(`Dear ${guests}, you're still invited to dinner.`)
+// );
+// // Removing the last two names from the list
+// guest.pop();
+// guest.pop();
+// // Print the final list to the confirm it's empty
+// console.log("Final guest list: ", guest)
+// // ----------Task 18------------
+// // Define the array of locations
+// let places = ["Tokyo", "Machu Picchu", "Santorini", "Banff", "Venice"];
+// // Print the original array
+// console.log("Original Order: ", places);
+// // Print the array in alphabetical order without modifying the original list
+// console.log("\nAlphabetical Order: ", [...places].sort());
+// // Show that the original array is still in its original order
+// console.log("\nOriginal Order after sorting: ", places);
+// // Print the array in reverse alphabetical order without changing the original list
+// console.log("\nReverse Alphabetical Order: ", [...places].sort().reverse());
+// // Show that the original array is still in its original order
+// console.log("\nOriginal Order after reverse sorting: ", places);
+// // Reverse the order of the list
+// places.reverse();
+// // Print the array to show its order has changed
+// console.log("\nReversed Order: ", places);
+// // Reverse the order of the list again to restore original order
+// places.reverse();
+// // Print the array to show it's back to its original order
+// console.log("\nBack to original Order: ", places);
+// // Sort the array in alphabetical order
+// places.sort();
+// // Print the array to show its order has been changed
+// console.log("\nSorted in Alphabetical Order: ", places);
+// // Sort the array in reverse alphabetical order
+// places.sort((a, b) => b.localeCompare(a));
+// // Print the array to show its order has been changed
+// console.log("\nSorted in Reverse Alphabetical Order: ", places);
+// // ----------Task 19------------
+var guestDinner = ["Ali", "Bilal"];
+var count_invitation = guestDinner.length;
+console.log(count_invitation, "people will come to the dinner.");
--- main.ts
+++ main.ts
@@ -106,46 +106,132 @@
 
 // // ----------Task 16------------
 
-let guestArr = ["Ali", "Ahmed", "Mehak", "Almas", "Bilal"];
+// let guestArr = ["Ali", "Ahmed", "Mehak", "Almas", "Bilal"];
 
-let canNotAttend = "Mehak";
+// let canNotAttend = "Mehak";
 
-let NewGuest = "Fahad";
+// let NewGuest = "Fahad";
 
-guestArr[guestArr.indexOf(canNotAttend)] = NewGuest;
+// guestArr[guestArr.indexOf(canNotAttend)] = NewGuest;
 
-console.log(guestArr);
+// console.log(guestArr);
 
-// guestArr.map((item)=> console.log(`Dear ${item}, I found a bigger dinner table so I am invited more peoples.`));
+// // guestArr.map((item)=> console.log(`Dear ${item}, I found a bigger dinner table so I am invited more peoples.`));
 
-// part 2 began
+// // part 2 began
 
-let guestBeg = "Muizz";
+// let guestBeg = "Muizz";
 
-guestArr.unshift(guestBeg);
+// guestArr.unshift(guestBeg);
 
-console.log(guestArr);
+// console.log(guestArr);
 
-// part 3 Middle
+// // part 3 Middle
 
-let guestMiddle = "Hamza";
+// let guestMiddle = "Hamza";
 
-let MiddleIndex = guestArr.length / 3;
+// let MiddleIndex = guestArr.length / 3;
 
-guestArr.splice(MiddleIndex, 0, guestMiddle);
+// guestArr.splice(MiddleIndex, 0, guestMiddle);
 
-console.log(guestArr);
+// console.log(guestArr);
 
-// part 4 append
+// // part 4 append
 
-guestArr.push("Zeeshan");
+// guestArr.push("Zeeshan");
 
-console.log(guestArr);
+// console.log(guestArr);
 
-// part 5
+// // part 5
 
-guestArr.map((items) =>
-  console.log(
-    `Dear ${items} you are invited in the more people category on dinner`
-  )
-);
+// guestArr.map((items) =>
+//   console.log(
+//     `Dear ${items} you are invited in the more people category on dinner`
+//   )
+// );
+
+// // ----------Task 17------------
+
+// let guest = ["Ali", "Bilal", "Carry", "Dawood", "Hamza"];
+
+// console.log("Due to limited space, only two people can be invited for dinner.")
+
+// // Removing guest until only two names remain
+
+// while (guest.length > 2){
+//   const removeGuest = guest.pop();
+//   console.log(`Sorry, ${removeGuest}, you're no longer invited to dinner.`)
+// }
+
+// // Print Invitations to the remaining two guest
+
+// let guests = ["Ali", "Bilal"];
+
+// guests.forEach((guests) =>
+//   console.log(`Dear ${guests}, you're still invited to dinner.`)
+// );
+
+// // Removing the last two names from the list
+
+// guest.pop();
+// guest.pop();
+
+// // Print the final list to the confirm it's empty
+// console.log("Final guest list: ", guest)
+
+// // ----------Task 18------------
+
+// // Define the array of locations
+// let places = ["Tokyo", "Machu Picchu", "Santorini", "Banff", "Venice"];
+
+// // Print the original array
+// console.log("Original Order: ", places);
+
+// // Print the array in alphabetical order without modifying the original list
+// console.log("\nAlphabetical Order: ", [...places].sort());
+
+// // Show that the original array is still in its original order
+// console.log("\nOriginal Order after sorting: ", places);
+
+// // Print the array in reverse alphabetical order without changing the original list
+// console.log("\nReverse Alphabetical Order: ", [...places].sort().reverse());
+
+// // Show that the original array is still in its original order
+// console.log("\nOriginal Order after reverse sorting: ", places);
+
+// // Reverse the order of the list
+// places.reverse();
+
+// // Print the array to show its order has changed
+// console.log("\nReversed Order: ", places);
+
+// // Reverse the order of the list again to restore original order
+// places.reverse();
+
+// // Print the array to show it's back to its original order
+// console.log("\nBack to original Order: ", places);
+
+// // Sort the array in alphabetical order
+// places.sort();
+
+// // Print the array to show its order has been changed
+// console.log("\nSorted in Alphabetical Order: ", places);
+
+// // Sort the array in reverse alphabetical order
+// places.sort((a, b) => b.localeCompare(a));
+
+// // Print the array to show its order has been changed
+// console.log("\nSorted in Reverse Alphabetical Order: ", places);
+
+// // ----------Task 19------------
+
+// let guestDinner = ["Ali", "Bilal"];
+// let count_invitation = guestDinner.length;
+
+// console.log(count_invitation , "people will come to the dinner.")
+
+
+// // ----------Task 20------------
+
+
+
