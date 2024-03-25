18bbea0bf618a6a5f20be0ba26a3efe2fad7dfdf
fahadmemon1234 <fahadmemon131@gmail.com>
2024-03-25 22:25:10 +0500

    add file

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+// const randomNumber = 13;
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number 1 to 6: ",
+    },
+]);
+if (answers.userGuessedNumber == randomNumber) {
+    console.log("Congratulaion! You guessed right now.");
+}
+else {
+    console.log("You guessed wrong number.");
+}
